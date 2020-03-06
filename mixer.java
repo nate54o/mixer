9afc83cb9e6c90547a849ac57080953ed0a3ab38
Nate <norage@msri.org>
2020-03-06 13:52:21 -0800

    test

---
+++ mixer.java
@@ -0,0 +1,17 @@
+import com.mixer.api.MixerAPI;
+import com.mixer.api.http.SortOrderMap;
+import com.mixer.api.resource.MixerUser;
+import com.mixer.api.resource.channel.MixerChannel;
+import com.mixer.api.response.channels.ShowChannelsResponse;
+import com.mixer.api.services.impl.ChannelsService;
+import com.mixer.api.services.impl.UsersService;
+
+import java.util.concurrent.ExecutionException;
+
+public class Tutorial {
+    public static MixerAPI mixer;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        mixer = new MixerAPI("Click here to get your Client ID!");
+    }
+}
